7364839b13018a098149a5b46f5f2aca9bab7842
shaack <shaack@gmail.com>
2018-03-01 22:41:29 +0100

    Initial commit

---
+++ src/Event.js
@@ -0,0 +1,34 @@
+/**
+ * Author: shaack
+ * Date: 29.12.2017
+ */
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function (s) {
+            let matches = (this.document || this.ownerDocument).querySelectorAll(s);
+            let i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {
+            }
+            return i > -1;
+        };
+}
+
+export class Event {
+    static delegate(element, event, selector, handler) {
+        element.addEventListener(event, function (event) {
+            let target = event.target;
+            while (target && target !== this) {
+                if (target.matches(selector)) {
+                    handler.call(target, event);
+                }
+                target = target.parentNode;
+            }
+        });
+    }
+}
